90d70cac2b294a25fb295815b78035bac298ef01
bgsphamster <hamstercubeworoom@yeah.net>
2023-02-02 19:38:41 +0800

    添加了gameboard.py并小修其他源码

--- snake.py
+++ snake.py
@@ -32,7 +32,6 @@ class Snake:
         # 先验证新位置合法性，要求不是身体的一部分，不超过边界
         if MAX_GAMEBOARD_SIZE >= next[X] > 0 and MAX_GAMEBOARD_SIZE >= next[Y] > 0 and self.body.count(next) > 0:
             # 把最后一节身体移到移动的目标位置，形成新的蛇头
-            original_last = self.body[len(self.body)-1]
             self.body.pop()
             self.body.insert(0, next)
             return
